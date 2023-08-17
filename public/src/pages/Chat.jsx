08b1252fc56b4faf4cf4df54c11857ad1107642b
Siddharth Mishra <20uec130@lnmiit.ac.in>
2023-08-17 17:49:45 +0530

    first commit

---
+++ public/src/pages/Chat.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+import styled from "styled-components";
+import { allUsersRoute, host } from "../utils/APIRoutes";
+import ChatContainer from "../components/ChatContainer";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+
+export default function Chat() {
+  const navigate = useNavigate();
+  const socket = useRef(); // useRef update the data but the there will be no change seen in the component
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(async () => {
+    if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/login");
+    } else {
+      setCurrentUser(
+        await JSON.parse(
+          localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+        )
+      );
+    }
+  }, []);
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);//This line emits a custom event called "add-user" to the WebSocket server. It sends the currentUser's _id (likely a user identifier) as data along with the event. This indicates that the current user is being added or connected to the WebSocket server.
+    }
+  }, [currentUser]);// The function inside the hook will be executed when the component mounts, and any time the value of currentUser changes.
+
+  useEffect(async () => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        const data1 = await axios.get(`${allUsersRoute}/${currentUser._id}`);//Automatic Generation:* If you don't provide an `_id` value when inserting a new document, MongoDB will automatically generate a unique identifier for you.
+
+        setContacts(data1.data);
+      } else {
+        navigate("/setAvatar");
+      }
+    }
+  }, [currentUser]);
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+  return (
+    <>
+      <Container>
+        <div className="container">
+          <Contacts contacts={contacts} changeChat={handleChatChange} />
+          {currentChat === undefined ? (
+            <Welcome />
+          ) : (
+            <ChatContainer currentChat={currentChat} socket={socket} />
+          )}
+        </div>
+      </Container>
+    </>
+  );
+}
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
